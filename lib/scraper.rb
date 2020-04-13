19bba72f6da2634d5e3b3707572bb0ff79402843
Ryan Parrish <rino3717@gmail.com>
2020-04-13 01:15:24 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,45 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
+
 
 class Scraper
-
+    
   def self.scrape_index_page(index_url)
     
+    students = []
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    index.css("div.student-card").each do |student|
+      profile = {}
+      profile[:name] = student.css("h4.student-name").text 
+      profile[:location] = student.css("p.student-location").text 
+      profile[:profile_url] = student.css("a").attribute("href").value
+      students << profile 
+      
+    end
+    students  
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student_profile = {}
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
+    # Social Links
+    profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_profile[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
+      end
+    end
+    student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
+    student_profile
   end
 
 end
